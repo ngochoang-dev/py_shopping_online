0c50f687f2086186b13843ae1c36cf92cc7c4364
ngochoangdev <ngochoang.dev@gmail.com>
2024-03-01 17:25:16 +0700

    authen

--- products/views.py
+++ products/views.py
@@ -1,28 +1,9 @@
-from rest_framework.viewsets import  ModelViewSet, generics
+from rest_framework.viewsets import  ModelViewSet
 from rest_framework.decorators import action
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework import permissions
 
 from .models import Product
 from .serializers import ProductSerializer
 
-class PermissionCustomer:
-    def has_permission(self, request, view):
-        return True
-
 class ProductListView(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [PermissionCustomer]
-
-    @action(methods=['get'], detail=True, permission_classes=[permissions.IsAdminUser])
-    def get_desc(self, request, pk=None):
-        try:
-            queryset =  Product.objects.get(pk=pk)
-            ser = ProductSerializer(queryset).data.get("description")
-    
-            return Response(ser, status=200)
-        except Product.DoesNotExist:
-            return Response({"message": "Not found"}, status=404)
-        
--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,14 @@
+from typing import Any, Dict
 from rest_framework.serializers import ModelSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from .models import User
 
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ["user_id", "username", "email", "phone"]
+
+class UserSignInSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs: Dict[str, Any]) -> Dict[str, str]:
+        print("XX", attrs)
+        return super().validate(attrs)
